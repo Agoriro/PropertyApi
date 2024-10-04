7bb47005d59e4ce949670f73dd2f73242336571d
Edward Martinez <Alejed@gmail.com>
2024-10-04 09:36:41 -0500

    Se agrega la documentación

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,6 +4,14 @@ from src.config import DatabaseConfig
 
 @pytest.fixture
 def db_config():
+    """
+    Fixture que proporciona la configuración de la base de datos para las pruebas.
+
+    Retorna:
+    --------
+    DatabaseConfig
+        Un objeto DatabaseConfig con los detalles de conexión a la base de datos de prueba.
+    """
     return DatabaseConfig(
         host='localhost',
         port=3306,
@@ -14,4 +22,17 @@ def db_config():
 
 @pytest.fixture
 def mock_db(mocker):
+    """
+    Fixture que proporciona una instancia mock de la clase Database utilizando el mocker de pytest.
+
+    Parámetros:
+    -----------
+    mocker : pytest-mock
+        Inyección automática del objeto mocker proporcionado por la librería pytest-mock.
+
+    Retorna:
+    --------
+    mocker.Mock
+        Un objeto mock que simula la clase Database.
+    """
     return mocker.Mock(spec=Database)
